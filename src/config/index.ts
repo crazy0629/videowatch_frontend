ff874b4ec8764e721bb2c70a3e72788f29a05ac3
crazy0629 <milan.jansen0629@gmail.com>
2024-01-29 14:14:27 +0900

    change port

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-export const SERVER_URI = "http://3.89.134.159:8000/api";
+export const SERVER_URI = "http://3.89.134.159:8002/api";
 export const SERVER_UPLOAD_URI = "http://3.89.134.159:8002";
 // export const SERVER_URI = "http://192.168.147.51:8002/api";
 // export const SERVER_UPLOAD_URI = "http://192.168.147.51:8002";
