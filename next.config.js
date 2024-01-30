1b7333f0641639fcdd2ae61202ea86ebd847af8e
crazy0629 <milan.jansen0629@gmail.com>
2024-01-30 22:43:24 +0900

    change : server url

--- next.config.js
+++ next.config.js
@@ -6,9 +6,9 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "http",
-        // hostname: "192.168.146.181",
-        hostname: "3.89.134.159",
-        port: "8002",
+        // hostname: "localhost",
+        hostname: "54.198.206.126",
+        port: "8000",
       },
     ],
   },
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,8 @@
-export const SERVER_URI = "http://3.89.134.159:8002/api";
-export const SERVER_UPLOAD_URI = "http://3.89.134.159:8002";
+export const SERVER_URI = "http://54.198.206.126:8000/api";
+export const SERVER_UPLOAD_URI = "http://54.198.206.126:8000";
+export const INIT_URL = "http://54.198.206.126:3000";
+
 // export const SERVER_URI = "http://192.168.147.51:8002/api";
 // export const SERVER_UPLOAD_URI = "http://192.168.147.51:8002";
-export const INIT_URL = "http://3.89.134.159:3001";
 
 export const ADMIN_INFO = "admin12345";
